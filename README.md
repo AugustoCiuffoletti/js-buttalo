baf7aa4d74726472e0fd63fe26bfd8e2df931232
AugustoCiuffoletti <augusto@di.unipi.it>
2021-01-14 10:46:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-yktbmc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yktbmc)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+document.getElementById("demo").innerHTML = "Primo<br>";
+document.getElementById("demo").innerHTML += "Secondo<br>";
+document.getElementById("demo").innerHTML += "<b>Terzo</b>";
